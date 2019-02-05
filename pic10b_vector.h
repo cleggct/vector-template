843dd73bdd236571d70c22a41f23112067fe615f
unknown <cleggct@gmail.com>
2019-02-05 00:00:15 -0800

    more bug fixes

--- pic10b_vector.h
+++ pic10b_vector.h
@@ -189,13 +189,13 @@ std::ostream& operator<<( std::ostream& out, const Pic10b::vector<T>& v ){
 }
 
 template<typename T>
-T& operator*(const Pic10b::vector<T>& lhs, const Pic10b::vector<T>& rhs) {
+T operator*(const Pic10b::vector<T>& lhs, const Pic10b::vector<T>& rhs) {
 	int min_sz = lhs.size() < rhs.size() ? lhs.size() : rhs.size();
 	T result = T();
 	for (size_t i = 0; i < min_sz; ++i) {
 		result += (lhs[i] * rhs[i]);
 	}
-	return *result;
+	return result;
 }
 
 template<typename T>
@@ -223,11 +223,11 @@ Pic10b::vector<T> operator+(Pic10b::vector<T> lhs, const Pic10b::vector<T>& rhs)
 }
 
 template<typename T>
-Pic10b::vector<T>& operator+=(Pic10b::vector<T>& lhs, const Pic10b::vector<T>& rhs) {
+Pic10b::vector<T> operator+=(Pic10b::vector<T>& lhs, const Pic10b::vector<T>& rhs) {
 	for (size_t i = 0; i < lhs.size(); ++i) {
 		lhs[i] += rhs[i];
 	}
-	return *lhs;
+	return lhs;
 }
 
 template<typename T>
